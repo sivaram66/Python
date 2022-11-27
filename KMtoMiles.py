ab0a86529dd69f831b1f625d60147a9ad29bc1fe
Siva Ram <sivaramlikeu@gmail.com>
2022-11-27 14:58:52 +0530

    KilometerToMiles

---
+++ KMtoMiles.py
@@ -0,0 +1,6 @@
+#1.609 kilometers equal 1 mile. 
+
+KM = eval(input("Enter the Kilometers"))
+
+Mile= (1.609)*KM
+print(Mile)
---
+++ Randomvariable.py
@@ -0,0 +1,4 @@
+#To Generate Random Number in python 'randint()' function is used
+
+import random
+print(random.randint(0,6))
