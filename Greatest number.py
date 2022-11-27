8e7272c3ce6595afa92f3f62675dd2ef79369648
Siva Ram <sivaramlikeu@gmail.com>
2022-11-27 13:46:57 +0530

    Greatest  Number

--- Greatest number.py
+++ Greatest number.py
@@ -1,5 +1,5 @@
-x = int(input("enter the 1st Number"))
-y = int(input("enter the 2nd number"))
+x = int(input("Enter the 1st Number"))
+y = int(input("Enter the 2nd number"))
 if x >y:
     print(f"{0} is Greatest the  Number")
 else: 
