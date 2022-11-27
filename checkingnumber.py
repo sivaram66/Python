beeec9f704946fe9b7bdcf6ca14b24bae59f8a31
Siva Ram <sivaramlikeu@gmail.com>
2022-11-27 22:28:55 +0530

    Checking whether the number is Positive or Negative

---
+++ checkingnumber.py
@@ -0,0 +1,14 @@
+num = eval(input("enter the number"))
+if  num>0:
+    print("{} is Positive Number".format(num))
+elif num ==0:
+    print("Zero")
+if num<0:
+    print("{} is Negative Number".format(num))
+
+
+
+#The upper one is Called (Nested if) means [if inside if]
+
+#we can also do this in another method using {elif}
+
