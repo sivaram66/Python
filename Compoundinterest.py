3a6d8839da4973975a4697434a8ebf2c9ac00d1c
Siva Ram <sivaramlikeu@gmail.com>
2022-11-27 13:45:16 +0530

    compoundinterest

--- Compoundinterest.py
+++ Compoundinterest.py
@@ -1,9 +1,9 @@
-Pr = float(input("enter the Principal amount"))
+PR = float(input("enter the Principal amount"))
 rate =float(input('Enter the Rate'))
-num= int(input('Number of times interest is compounded per year'))
+num= float(input('Number of times interest is compounded per year'))
 time=float(input('no of years'))
 
-Amount = Pr*(1+rate/num)**(num*time)
+Amount = PR*(1+rate/num)**(num*time)
 print(Amount)
-CI = Amount - Pr
+CI = Amount - PR
 print(CI)
