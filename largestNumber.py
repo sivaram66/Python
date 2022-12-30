d8580839d1d3be60e8c5b90b9683bad7c4c649ec
Siva Ram <sivaramlikeu@gmail.com>
2022-12-30 10:22:28 +0530

    LargestNumber

--- largestNumber.py
+++ largestNumber.py
@@ -3,7 +3,7 @@ Num2 = eval(input("Enter the 2nd Number"))
 Num3 = eval(input("Enter the 3nd Number"))
 if (Num1 >Num2) and (Num1 >Num3):
     print("{} is the Largest Number".format(Num1))
-if (Num2 >Num1) and (Num2 > Num3):
+if (Num2>Num1) and (Num2 > Num3):
     print("{} is the Largest Number".format(Num2))
 if(Num3 >Num1) and  (Num3 >Num2):
     print("{} is the Largest Number".format(Num3))
