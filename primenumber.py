eb84f4b686c49913d33cfb97ca12ba298e31b374
Siva Ram <sivaramlikeu@gmail.com>
2023-01-02 12:37:22 +0530

    primenumber.py

--- primenumber.py
+++ primenumber.py
@@ -1,1 +1,11 @@
-hii 
+x = eval(input("Enter The Number "))
+
+if x ==1:
+    print(x,"is not a prime number")
+elif x >1:
+    for i in range(2,x):
+        if x%i==0:
+            print("{} is Not a Prime Number".format(x))
+            break
+    else:
+            print("{} is a prime Number".format(x))
