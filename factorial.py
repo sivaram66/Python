e231581e80c3414bd84495e1d67346b1360340aa
Siva Ram <sivaramlikeu@gmail.com>
2023-01-02 11:32:07 +0530

    factorial.py

--- factorial.py
+++ factorial.py
@@ -1,7 +1,10 @@
+n = eval(input("Enter the Number"))
+
 def fac(n):
-    if n==0:
-      return 1
-    return n*fac(n-1)
-    
-result = fac(4)
+  if n ==0:
+    return 1
+  return n *fac(n-1)
+
+
+result = fac(n)
 print(result)
